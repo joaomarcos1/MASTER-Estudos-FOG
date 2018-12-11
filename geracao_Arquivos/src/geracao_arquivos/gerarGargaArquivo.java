e1eeaae112ee1411e8927f4926f7ddeded2449a9
joaomarcos1 <rosaljoao@gmail.com>
2018-12-11 10:38:19 -0300

    CorreçõesGEração de Palavras

--- geracao_Arquivos/src/geracao_arquivos/gerarGargaArquivo.java
+++ geracao_Arquivos/src/geracao_arquivos/gerarGargaArquivo.java
@@ -27,25 +27,32 @@ public class gerarGargaArquivo {
 
         //System.out.println("Inserir quantidade de palavras:");
         //quantidade = ler.nextInt();
-
         //String article[] = {"the", "a", "one", "some", "any"};
         //String noun[] = {"boy", "the", "dog", "town", "car"};
         //String verb[] = {"drove", "jumped", "ran", "walked", "skipped"};
         //String preposition[] = {"to", "from", "over", "under", "on"};
-
         String article[] = {"the"};
         String noun[] = {"the"};
         String verb[] = {"drove"};
         String preposition[] = {"over"};
+
+        String article2[] = {"one"};
+        String article3[] = {"any"};
+
+        String noun2[] = {"dog"};
+        String noun3[] = {"boy"};
         
+        String verb2[] = {"walked"};
+        String verb3[] = {"jumped"};
         
-        
+        String preposition2[] = {"from"};
+        String preposition3[] = {"under"};
+
         String conteudo;
         ArrayList<String> texto = new ArrayList<>();
 
         //GERANDO VALORES ALEATÓRIOS APENAS UMA VEZ PARA CADA PALAVRA
         //SERÃO GERADOS APENAS AS MEMAS PALAVRAS NO ARQUIVO
-        
         int articleLength = article.length;
         int nounLength = noun.length;
         int verbLength = verb.length;
@@ -57,21 +64,19 @@ public class gerarGargaArquivo {
         int randPreposition = (int) (Math.random() * prepositionLength);
         int randArticle2 = (int) (Math.random() * articleLength);
         int randNoun2 = (int) (Math.random() * nounLength);
-        
+
         int randArticle10 = (int) (Math.random() * articleLength);
         int randNoun10 = (int) (Math.random() * nounLength);
         int randVerb10 = (int) (Math.random() * verbLength);
         int randPreposition10 = (int) (Math.random() * prepositionLength);
 
-        
         int qt_megas = Integer.valueOf(args[0]);
-        
-        
-        for (i = 0; i < ((qt_megas * 225000)/ 10); i++) {
+
+        for (i = 0; i < ((qt_megas * 225000) / 10); i++) {
             //tamanhos totais palavras de cada tipo 
 
-            frase = article[randArticle] + " " + noun[randNoun] + " " + verb[randVerb] + " " + preposition[randPreposition] + " " + article[randArticle] + " " + noun[randNoun2] + " " + article[randArticle10] + " " + noun[randNoun10] + " " + verb[randVerb10] + " " + preposition[randPreposition10] + " ";
-      
+            frase = article[randArticle] + " " + noun[randNoun] + " " + verb[randVerb] + " " + preposition[randPreposition] + " " + article2[0] + " " + noun2[0] + " " + article3[0] + " " + noun[randNoun10] + " " + verb3[0] + " " + preposition3[0] + " ";
+
             texto.add(frase);
 
         }
@@ -85,5 +90,4 @@ public class gerarGargaArquivo {
 
     }
 
-   
 }
